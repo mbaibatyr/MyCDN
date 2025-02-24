8d0c7a2e27b83ca3e9f346f347632368bb27fe54
mbaibatyr <murat_b@mail.ru>
2025-02-24 21:42:14 +0500

    123

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var root = ReactDOM.createRoot(document.getElementById("app"));
 function F1() {
+    const title = "STEP";
+    const title2 = "STEP2222222";
     if (1 == 2)
-        return <h2>Hello 1</h2>
+        return <h2>Hello {title}</h2>
     else
-        return <h2>Hello 2</h2>
+        return <h2>Hello {title2}</h2>
 }
 
 // class F2 extends React.Component {
@@ -12,9 +14,14 @@ function F1() {
 // }
 // }
 root.render(
-    <div>
+    <>
         привет STEP 1234
-        <h2>2 + 2 = {2 + 2}</h2>
+        {/* <h2>2 + 2 = {2 + 2}</h2> */}
         <F1 />
-    </div>
+        <F1 />
+        <F1 />
+        <F1 />
+        <p className=""></p>
+    </>
+
 );
